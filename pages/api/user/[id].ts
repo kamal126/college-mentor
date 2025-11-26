7e025e6bd5b1dc6e26f5c530c9f7f6b4161aeeff
kamal126 <kamalchandra265@gmail.com>
2025-11-26 21:56:30 +0530

    id generte

---
+++ pages/api/user/[id].ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectDB from "../../../lib/db";
+import { User } from "../../../models/user.model";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await connectDB();
+
+  const { id } = req.query;
+
+  if(req.method === "GET") {
+    const user = await User.findById(id);
+    return res.status(200).json(user);
+  }
+
+  if(req.method === "PUT") {
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
+    return res.status(200).json(updatedUser);
+  }
+
+  res.status(405).json({ message: "Method not allowed" });
+}
