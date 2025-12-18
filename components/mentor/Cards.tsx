d3f716ad078c417c02f6e718022e7c8c67988213
kamal126 <kamalchandra265@gmail.com>
2025-12-18 21:07:38 +0530

    cards for mentord

---
+++ components/mentor/Cards.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { fetchFilteredMentors } from "@/lib/data";
+import MentorCard from "../MentorCard";
+
+export default async function Cards({query, page}:{query:string; page:number}) {
+    const mentors = await fetchFilteredMentors(query, page);
+  return (
+    <div className="">
+      {mentors.map((mentor)=>(
+        <MentorCard key={mentor.id} mentor={mentor}/>
+      ))}
+    </div>
+  )
+}
