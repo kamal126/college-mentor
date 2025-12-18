d8e8a34e1b259df17ceb9c2e0c43a0ab0649d6be
kamal126 <kamalchandra265@gmail.com>
2025-12-18 21:11:22 +0530

    update file name db -> connectDB

---
+++ lib/connectDB.ts
@@ -0,0 +1,40 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI =
+  process.env.DATABASE_URL + process.env.DATABASE_NAME;
+
+if(!MONGODB_URI){
+    throw new Error("Please define DATABASE_URI and DATABASE_NAME in .env");
+}
+
+// Global cache (prevents multiple connections in dev & production)
+
+declare global {
+    // eslint-disable-next-line no-var
+    var mongoose : {
+        conn: Mongoose | null;
+        promise: Promise<Mongoose> | null;
+    };
+}
+
+let cached = global.mongoose;
+
+if(!cached){
+    cached = global.mongoose = {conn: null, promise:null};
+}
+
+const connectDB = async () : Promise<Mongoose> => {
+    if( cached.conn ) return cached.conn;
+
+    // create onnection once
+    if(!cached.promise){
+        cached.promise = mongoose.connect(MONGODB_URI,{bufferCommands: false});
+    }
+
+    cached.conn = await cached.promise;
+    console.log("MongoDB connected");
+
+    return cached.conn;
+};
+
+export default connectDB;
