208181e92d9e6cd7c9d594169ddb2eac78b8d3f1
kamal126 <kamalchandra265@gmail.com>
2025-12-18 21:04:22 +0530

    working

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -43,23 +43,7 @@ export default function HomePage() {
     <section className="">
       {/* search bar */}
       <nav className="flex sticky top-0">
-        <div className="max-w-xl mx-auto sticky top-0 bg-gray-300 rounded-full px-4 py-2 flex items-center gap-3">
-          <input
-            type="text"
-            placeholder="Search mentors..."
-            maxLength={200}
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            onKeyDown={(e) => e.key === "Enter" && handleSearch()}
-            className="flex-1 bg-transparent outline-none px-2"
-          />
-          <button
-            onClick={handleSearch}
-            className="px-5 py-2 rounded-full border border-gray-600 hover:bg-gray-400 transition"
-          >
-            Search
-          </button>
-        </div>
+        
 
         <button onClick={handleHam} className="md:hidden z-50 p-2 rounded">
           ☰
@@ -102,77 +86,7 @@ export default function HomePage() {
         </div>
 
         {/* Main Content (Scrollable) */}
-        <div className="ml-3 m-2 w-full">
-          <div className="">
-            {filteredMentors.length > 0 ? (
-              filteredMentors.map((m, idx) => (
-                <div
-                  key={idx}
-                  className="text-black dark:text-white border-3 hover:shadow-2xl p-6 space-y-4 mt-2 flex flex-col gap-4 md:flex-row rounded-2xl"
-                >
-                  <div className="">
-                    <div className="flex gap-4">
-                      <div className="">
-                        <img
-                          src="https://www.geeksforgeeks.org/connect/_next/image?url=https%3A%2F%2Fmedia.geeksforgeeks.org%2Fauth%2Fprofile%2Fawaxza35wh9rn0zonena&w=96&q=75"
-                          alt=""
-                          height={100}
-                          width={100}
-                          className="rounded-full border border-green-700 border-4 bg-gray-500"
-                        />
-                      </div>
-                      <div className="">
-                        <h3 className="">{m.name}</h3>
-                        <p className="">
-                          {m.title} @ {m.company}
-                        </p>
-                        <p className="">
-                          Companies:{" "}
-                          <span>
-                            {m.companies.map((company) => company).join(", ")}
-                          </span>
-                        </p>
-                        <p className="">{m.experience} years experience</p>
-                      </div>
-                    </div>
-
-                    <div className="">
-                      <div className="">
-                        <p className="">
-                          I am a content creator , I create content around
-                          education, career and Tech
-                        </p>
-                        <span className="bg-gray-400 p-1 border border-white rounded-2xl mx-1">
-                          Career Guidance
-                        </span>
-                        <span className="bg-gray-400 p-1 border border-white rounded-2xl mx-1">
-                          Career Guidance
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="">
-                    <div className="flex justify-between bg-gray-300 p-4">
-                      <div className="">Rating : 4.5 ⭐</div>
-                      <div className="">Price : $10/hr</div>
-                    </div>
-                    <div className="">
-                      <button className="bg-blue-500 text-white p-2 rounded-lg w-full mt-2">
-                        View Profile
-                      </button>
-                      <button className="bg-blue-500 text-white p-2 rounded-lg w-full mt-2">
-                        Connect
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              ))
-            ) : (
-              <p>No mentors found.</p>
-            )}
-          </div>
-        </div>
+        
 
         {/* Right Sidebar */}
         <div className=" sticky top-10 h-screen hidden md:block">
