92f046911742ef7bb4dbd331fd12ebf0cef4426f
kamal126 <kamalchandra265@gmail.com>
2025-12-21 22:28:50 +0530

    add: clodinary file upload utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,38 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
 export function cn(...inputs: ClassValue[]){
     return twMerge(clsx(inputs));
 }
 
 // ============================================
+export async function uploadOnCloudinary (localFilePath:string) {
+    try {
+        if(!localFilePath) return null;
+
+        const path = localFilePath.replace(/\\/g,"/");
+
+        const response = await cloudinary.uploader.upload(path,{
+            resource_type: "auto",
+        });
+        console.log("File uploaded successfully: ", response.url);
+
+        fs.unlink(path, (err)=>{
+            if(err) console.error("Failed to delete temp file: ", err);
+        });
+        
+        return response;
+        
+    } catch (error) {
+        console.log("Cloudinary upload error:",error);
+
+        fs.unlink(localFilePath, (err)=>{
+            if(err) console.error("Failed to delete temp file:",err);
+        });
+
+        return null;
+    }
+}
