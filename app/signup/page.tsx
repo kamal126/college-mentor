1b03d1e382b3d6e161322c9a5bead1f4a9d23258
kamal126 <kamalchandra265@gmail.com>
2025-12-26 13:00:16 +0530

    add: signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+"use client";
 
-export default function page() {
+// import { useFormState } from "react-dom";
+import SignupForm from "@/components/signup-form";
+import { Suspense } from "react";
+export default function Page() {
+
   return (
-    <div>page</div>
-  )
+    <main className="flex flex-col w-full h-screen justify-center items-center bg-blue-100">
+      <div className="border-2 border-slate-300 p-10 rounded-lg bg-white">
+        <div className="flex justify-center mb-3 font-semibold rounded-sm bg-blue-300">
+          <h2 className="text-2xl">Sign Up</h2>
+        </div>
+        <Suspense>
+            <SignupForm/>
+        </Suspense>
+      </div>
+    </main>
+  );
 }
