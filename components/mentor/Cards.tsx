a38e7fb0d62b256230b1b66e34cb1c30a81d93c5
kamal126 <kamalchandra265@gmail.com>
2025-12-26 13:08:10 +0530

    update: mentor card key

--- components/mentor/Cards.tsx
+++ components/mentor/Cards.tsx
@@ -6,8 +6,8 @@ export default async function Cards({query, page}:{query:string; page:number}) {
     const mentors = await fetchFilteredMentors(query, page);
   return (
     <div className="">
-      {mentors.map((mentor)=>(
-        <MentorCard key={mentor._id} mentor={mentor}/>
+      {mentors.map((mentor,i)=>(
+        <MentorCard key={i} mentor={mentor}/>
       ))}
     </div>
   )
