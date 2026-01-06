f2f73e7a5410d2aac2c413580267f8dcf20f6034
kamal126 <kamalchandra265@gmail.com>
2026-01-06 22:25:39 +0530

    fix:  promise type

--- app/dashboard/mentor/[id]/Page.tsx
+++ app/dashboard/mentor/[id]/Page.tsx
@@ -5,9 +5,9 @@ import MentorProfileSkeleton from "@/components/ui/skeleton";
 export default async function MentorPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { id } =  params;
+  const { id } = await params;
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100">
       <Suspense fallback={<MentorProfileSkeleton />}>
