feb2b2c843786432cfaddb5b08a2a0aef8432d91
kamal126 <kamalchandra265@gmail.com>
2026-01-23 00:59:12 +0530

    fix: add pagination and mentor count fetching

--- app/dashboard/mentor/page.tsx
+++ app/dashboard/mentor/page.tsx
@@ -3,6 +3,8 @@ import Cards from "@/components/mentor/Cards";
 import { lusitana } from "@/components/font";
 import { Suspense } from "react";
 import { MentorCardsSkeleton } from "@/components/ui/skeleton";
+import { fetchFilteredMentorsCount } from "@/lib/data";
+import Pagination from "@/components/mentor/Pagination";
 
 export default async function Page({
   searchParams,
@@ -17,6 +19,9 @@ export default async function Page({
   const query = resolvedSearchParams?.query ?? "";
   const currentPage = Number(resolvedSearchParams?.page) || 1;
 
+  const total = await fetchFilteredMentorsCount(query);
+  const hasNextPage = currentPage*10<total; // assuming 10 per page
+  
   return (
     <main>
       <div className="w-full">
@@ -39,6 +44,7 @@ export default async function Page({
 
         <div className="mt-5 flex w-full justify-center">
           {/* Pagination later */}
+          <Pagination hasNextPage={hasNextPage}/>
         </div>
       </div>
     </main>
